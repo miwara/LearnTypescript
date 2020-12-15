7b08cf466f8900ed501ee8d6a5830763cadc0be0
Kodai Miura <miura.kodai@tixplus.co.jp>
2020-12-15 11:11:01 +0900

    classとinterfaceを試した

--- app.ts
+++ app.ts
@@ -1,2 +1,21 @@
-const hoge = (arg: number) => console.log(arg + 1)
-hoge(1)
+class VirtualPoint {
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+interface Point {
+    x: number;
+    y: number;
+}
+
+function printPoint(p: Point) {
+    console.log(`${p.x}, ${p.y}`);
+}
+
+const newVPoint = new VirtualPoint(13, 56);
+printPoint(newVPoint);
